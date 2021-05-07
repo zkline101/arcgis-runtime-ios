387499d7169e4db4dfd2f001c009217ecba068b4
Zachary Kline <zkline@esri.com>
2021-05-07 12:05:45 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -38,8 +38,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ArcGIS",
-            url: "https://www.dropbox.com/s/d0emq68wjrkzogo/ArcGIS-iOS-v100.11.1.xcframework.zip?dl=1",
-            checksum: "fdcdd842a3dacbb5252b501dbff6ea8be7ef50eb2cfc3fda17a82c07fd2eb16e"
+            url: "https://www.dropbox.com/s/k21niah1g478vnc/ArcGIS-iOS-v100.11.1.xcframework.zip?dl=1",
+            checksum: "89c94ca78a16a67b4e61780b1dc2cac7b4effcc480975e1d7c9dd171086535a4"
         )
     ]
 )
